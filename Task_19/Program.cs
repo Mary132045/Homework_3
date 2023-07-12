1ae1f0100facb60f144d899dfae5fc78b4134168
Машенька <Mary030289@yandex.ru>
2023-07-12 17:06:31 +0300

    Добавили файлы с программами домашнего задания 3 семинара

---
+++ Task_19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Int32.Parse(Console.ReadLine());
+
+int[] arr = num.ToString().ToCharArray().Select(x => x - '0').ToArray();
+
+if (arr[0] == arr[4] && arr[1] == arr[3])
+{
+    Console.WriteLine("Введенное число является палиндромом!");
+}
+else
+{
+    Console.WriteLine("Введенное число не является палиндромом!");
+}
